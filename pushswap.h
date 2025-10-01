801be6f55049c05713070fa116d9c071e93423c9
Ousmane Diallo <ousdiall@f1br3s4.clusters.42paris.fr>
2025-10-01 18:25:25 +0200

    Premier prototype du header en utilisant liste chainee pour la stack

---
+++ pushswap.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pushswap.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ousdiall <ousdiall@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/01 16:41:17 by ousdiall          #+#    #+#             */
+/*   Updated: 2025/10/01 16:48:45 by ousdiall         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# ifndef PUSHWAP_h
+# define PUSHSWAP_h
+
+#include <unistd.h>
+#include <stdlib.h>
+#include "libft.h"
+
+typedef struct s_node
+{
+	int value;
+	int index;
+	struct s_node *next;
+} t_node;
+
+
+#endif
